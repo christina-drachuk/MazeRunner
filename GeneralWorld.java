87fe21904757cafecbd13e0b2b7c3db7e0d61749
CHRISTINA DRACHUK <cdrachuk@gmail.com>
2021-06-08 20:31:33 -0400

    made general world subclass

---
+++ GeneralWorld.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class GeneralWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GeneralWorld extends World
+{
+
+    /**
+     * Constructor for objects of class GeneralWorld.
+     * 
+     */
+    public GeneralWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(1700, 800, 1);
+    }
+}
--- MyWorld.java
+++ MyWorld.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class MyWorld extends World
+public class MyWorld extends GeneralWorld
 {
 
     /**
@@ -16,7 +16,6 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(1700, 800, 1);
         prepare();
     }
 
