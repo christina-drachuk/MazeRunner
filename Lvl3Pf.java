bdc41ec235fb200485e1104f04e9cf67e10f5590
CHRISTINA DRACHUK <cdrachuk@gmail.com>
2021-06-14 23:36:53 -0400

    Merge branch 'master' of https://github.com/christina-drachuk/MazeRunner

---
+++ Lvl3Pf.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Lvl3Pf here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Lvl3Pf extends Actor
+{
+    /**
+     * Act - do whatever the Lvl3Pf wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
